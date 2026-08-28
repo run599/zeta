21f332f88f4da58fcfd55a77de87fcd6c2545f4c
run599 <run084533@qq.com>
2026-08-28 21:39:55 +0800

    Delete CppLibs directory

--- CppLibs/build_cmake.ps1
+++
@@ -1,2 +0,0 @@
-$vcvars = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
-cmd.exe /c ("`"$vcvars`" x64 > nul 2>&1 && cmake -S D:\ZETA\CppLibs -B D:\ZETA\CppLibs\build -G Ninja 2>&1")
--- CppLibs/zeta_engine/build_release.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" > nul 2>&1
-ninja -C "D:\ZETA\CppLibs\zeta_engine\build" -j8 2>&1
